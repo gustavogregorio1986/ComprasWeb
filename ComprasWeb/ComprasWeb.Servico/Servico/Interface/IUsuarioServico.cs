2948a7d6127cde1b87136485dfb92429719fafc3
guga56 <gustavogregorio086@gmail.com>
2023-05-22 15:18:54 -0300

    criara interface e serviços

---
+++ ComprasWeb/ComprasWeb.Servico/Servico/Interface/IUsuarioServico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComprasWeb.Servico.Servico.Interface
+{
+    public interface IUsuarioServico
+    {
+    }
+}
