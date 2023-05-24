04ca6d5ae37c7ff39a145a16ee11db4f29c5af43
guga56 <gustavogregorio086@gmail.com>
2023-05-24 12:18:08 -0300

    melhorando o projeto colocando o DTO

---
+++ ComprasWeb/ComprasWeb.Data/DTO/UsuarioDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComprasWeb.Data.DTO
+{
+    public class UsuarioDTO
+    {
+        public int IdUser { get; set; }
+
+        public string User { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Email { get; set; }
+
+        public string Perfil { get; set; }
+    }
+}
